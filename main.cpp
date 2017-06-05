cf6baf69ee48495a076cf4f2da974c42eff10bd0
KonnyLiz <pinchehugochan@gmail.com>
2017-06-05 04:46:18 +0000

    solidos con un intento de luces

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 //#include <SOIL/SOIL.h>
 //variable para rotar
 double rotate_z=0;
+GLfloat lightPos[4] = {1.0, 4.0, 7.5, 1.0}; // Luz direccional
 int width, height;  //variables para el ancho y alto
 void specialKeys( int key, int x, int y )
 {
@@ -19,6 +20,61 @@ void specialKeys( int key, int x, int y )
     glutPostRedisplay();
 
 }
+
+void piso(float amb1, float amb2, float amb3, float dif1, float dif2, float dif3, float esp1, float esp2, float esp3, float shi){
+
+	 GLfloat mat_ambient[] = { amb1, amb2, amb3, 1.0};
+    GLfloat mat_diffuse[] = { dif1, dif2, dif3, 1.0};
+    GLfloat mat_specular[] = {esp1, esp2, esp3, 1.0};
+    GLfloat mat_shininess[] = {shi};
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode( GL_MODELVIEW_MATRIX );
+
+    glLoadIdentity();
+
+     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+		 glPushMatrix();
+	glBegin(GL_POLYGON);
+	glNormal3f(-6,0,0);
+    glVertex3f(-6,0,0);
+    glNormal3f(6,0,0);
+    glVertex3f(6,0,0);
+    glNormal3f(6,0,30);
+    glVertex3f(6,0,30);
+    glNormal3f(-6,0,30);
+    glVertex3f(-6,0,30);
+    glEnd();
+    glPopMatrix();
+}
+
+void esfera(float amb1, float amb2, float amb3, float dif1, float dif2, float dif3, float esp1, float esp2, float esp3, float shi){
+	
+	 GLfloat mat_ambient[] = { amb1, amb2, amb3, 1.0};
+    GLfloat mat_diffuse[] = { dif1, dif2, dif3, 1.0};
+    GLfloat mat_specular[] = {esp1, esp2, esp3, 1.0};
+    GLfloat mat_shininess[] = {shi};
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode( GL_MODELVIEW_MATRIX );
+
+    glLoadIdentity();
+
+     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+   glPushMatrix();
+    //glColor3f(0.0, 1.0, 0.0);
+    glScalef(0.08,0.08,0.08);
+    glTranslatef(-27,5,10);
+    glutSolidSphere(20,20,20);
+    glPopMatrix();
+}
+
 void display(void)
 {
     //  Borrar pantalla y Z-buffer
@@ -27,6 +83,8 @@ void display(void)
 
     // Resetear transformaciones
     glLoadIdentity();
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
     //gira para en z para mover todo
     glRotatef( rotate_z, 0.0, 0.0, 1.0 );
    //lineas de orientación
@@ -42,25 +100,20 @@ void display(void)
     glColor3f(1,0,0);
     glVertex3f(-0.0f,0.f,2.0f);
     glEnd();
-    //Dibujando lineas
-
-    glBegin(GL_POLYGON);
-    glVertex3f(-6,0,0);
-    glVertex3f(6,0,0);
-    glVertex3f(6,-0,30);
-    glVertex3f(-6,-0,30);
-    glEnd();
-
     
+    //Dibujando piso
+    piso(0.0215, 0.1745, 0.0215, //ambiente
+                    0.07568, 0.61424, 0.07568, //difuso
+                    0.633, 0.727811, 0.633, //especular
+                    50.0); //shininess
        //esfera
-    glPushMatrix();
-    glColor3f(0.0, 1.0, 0.0);
-    glScalef(0.08,0.08,0.08);
-    glTranslatef(-27,5,10);
-    glutSolidSphere(20,20,20);
-    glPopMatrix();
+       esfera(0.0215, 0.1745, 0.0215, //ambiente
+                    0.07568, 0.61424, 0.07568, //difuso
+                    0.633, 0.727811, 0.633, //especular
+                    50.0); //shininess
+ 
 
-     //cubo
+    /* //cubo
     glPushMatrix();
     glColor3f(0.0, 1.0, 0.0);
     //glScalef(0.08,0.08,0.08);
@@ -125,23 +178,17 @@ void display(void)
     glTranslatef(-2.5,0,20);
     glutSolidDodecahedron();
     glPopMatrix();
-   
+   */
     glFlush();
     glutPostRedisplay();
 }
 
 void init(void)
 {
-	glShadeModel(GL_SMOOTH);
-    //GLfloat light_position[] = { -0.50, -0.50,-1.0,0 };
-   //GLfloat light_position[] = { 7, 1,40.0,1 };
-    //GLfloat light_position[] = { 1, 1,1,1 };
-    //GLfloat luz_ambiental[] = { 0.2, 0.2, 0.2, 10.0 };
-    //glEnable(GL_LIGHTING);
-    //glEnable(GL_LIGHT0);
+	//glShadeModel(GL_SMOOTH);
+    
     glDepthFunc(GL_LESS);
     glEnable(GL_DEPTH_TEST);
-  // glLightfv(GL_LIGHT0,GL_POSITION,light_position);
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glPushMatrix();
 
