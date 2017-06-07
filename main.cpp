6ec1ed91b197dfd276e1859d9e608a51b6568c22
KonnyLiz <pinchehugochan@gmail.com>
2017-06-07 03:48:34 +0000

    sombra

--- main.cpp
+++ main.cpp
@@ -3,9 +3,42 @@
 #include <stdio.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
-
+#include <SOIL/SOIL.h>   
 int width, height;  //variables para el ancho y alto
-void specialKeys( int key, int x, int y );
+
+GLfloat plano_s[4] = {1, 0, 0, 0};// s=x
+GLfloat plano_v[4] = {0, 1, 0, 0};// v=y
+GLuint texture[0];
+
+void tex(void){
+ texture[0] = SOIL_load_OGL_texture // load an image file directly as a new OpenGL texture
+      (
+          "6.bmp", //se carga la imagen
+          SOIL_LOAD_AUTO,
+          SOIL_CREATE_NEW_ID,
+          SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+      );
+      glEnable(GL_TEXTURE_2D);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+      // allocate a texture name
+      glBindTexture(GL_TEXTURE_2D, texture[0]);
+    
+    glBegin(GL_POLYGON);
+    //glColor4f(0.77, 0.78, 0.78, 0.5); //iron
+    glTexCoord2f(10,10);
+    glVertex3f(-6,0,0);
+    glTexCoord2f(0,10);
+    glVertex3f(6,0,0);
+    glTexCoord2f(10,0);
+    glVertex3f(6,-0,30);
+    glTexCoord2f(0,0);
+    glVertex3f(-6,-0,30);
+    glEnd();
+      
+}
 
 void display(void)
 {
@@ -17,19 +50,18 @@ void display(void)
     glLoadIdentity();
 
     //Dibujando piso
-
-    glBegin(GL_POLYGON);
-    glColor3f(0.12, 0.12, 0.12);
-    glVertex3f(-6,0,0);
-    glVertex3f(6,0,0);
-    glVertex3f(6,-0,30);
-    glVertex3f(-6,-0,30);
-    glEnd();
-
-    
-       //esfera
+   tex();
+   
+    //esfera 
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
+    glColor4f(0,0,0,1);
+    glScalef(0.08,0.08,0.08);
+    glTranslatef(-30,5,10);
+    glutSolidSphere(20,20,20);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor3f(1.0, 0.86, 0.57);//gold
     glScalef(0.08,0.08,0.08);
     glTranslatef(-27,5,10);
     glutSolidSphere(20,20,20);
@@ -37,15 +69,28 @@ void display(void)
 
      //cubo
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    //glScalef(0.08,0.08,0.08);
+    glColor4f(0,0,0,0.5);
+    glTranslatef(1.5,0,0);
+    glutSolidCube(2);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor3f(0.98, 0.82, 0.76); //cooper
     glTranslatef(2,0,0);
     glutSolidCube(2);
     glPopMatrix();
 
      //cono
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
+    glColor4f(0.0, 0.0, 0,0.5);
+    glScalef(0.08,0.08,0.08);
+    glRotatef(270,1,0,0);
+    glTranslatef(-33,-300,0);
+    glutSolidCone(15,15,15,15);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor3f(0.0, 1.0, 1.0); //blue
     glScalef(0.08,0.08,0.08);
     glRotatef(270,1,0,0);
     glTranslatef(-30,-300,0);
@@ -54,7 +99,14 @@ void display(void)
     
     //torus
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
+    glColor4f(0,0,0,0.5);
+    glScalef(1,1,1);
+    glTranslatef(1.7,0,10);
+    glutSolidTorus(0.3, 0.6, 10, 20);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor4f(0,1,0,1);//green
     glScalef(1,1,1);
     glTranslatef(2,0,10);
     glutSolidTorus(0.3, 0.6, 10, 20);
@@ -62,15 +114,29 @@ void display(void)
     
     //tetera
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-   glScalef(1,1,1);
+    glColor4f(0,0,0,0.5);
+    glScalef(1,1,1);
+    glTranslatef(1.7,0,17);
+    glutSolidTeapot(1);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor3f(1,0,0);//rojo
+    glScalef(1,1,1);
     glTranslatef(2,0,17);
     glutSolidTeapot(1);
     glPopMatrix();
 
 	//tetraedron
     glPushMatrix();
-    glColor3f(1.0, 0.0, 1.0);
+    glColor4f(0,0,0,0.5);
+    glScalef(0,0,0);
+    glTranslatef(-5.5,0,22);
+    glutSolidTetrahedron();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor4f(1,1,0,1);//yellow
     glScalef(1,1,1);
     glTranslatef(-5,0,22);
     glutSolidTetrahedron();
@@ -78,24 +144,44 @@ void display(void)
 
 	 //octahedron
     glPushMatrix();
-    glColor3f(0.0, 1.0, 1.0);
-   glScalef(1,1,1);
+    glColor4f(0,0,0,0.5);
+    glScalef(1,1,1);
+    glTranslatef(-0.3,0,22);
+    glutSolidOctahedron();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor4f(0,1,1,1);//cyan
+    glScalef(1,1,1);
     glTranslatef(0,0,22);
     glutSolidOctahedron();
     glPopMatrix();
     
      //icosahedron
     glPushMatrix();
-    glColor3f(0.0, 1.0, 0.0);
+    glColor4f(0,0,0,0.5);
+   glScalef(1,1,1);
+    glTranslatef(2.8,0,22);
+    glutSolidIcosahedron();
+    glPopMatrix();
+    
+glPushMatrix();
+    glColor4f(1,0,1,1);//magenta
    glScalef(1,1,1);
     glTranslatef(3,0,22);
     glutSolidIcosahedron();
     glPopMatrix();
     
-
     //dodecaedro
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
+    glColor4f(0,0,0,0.5);
+    glScalef(0.8,0.8,0.8);
+    glTranslatef(-3,0,20);
+    glutSolidDodecahedron();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor4f(1,1,1,1);//white
     glScalef(0.8,0.8,0.8);
     glTranslatef(-2.5,0,20);
     glutSolidDodecahedron();
@@ -107,19 +193,9 @@ void display(void)
 
 void init(void)
 {
-	glShadeModel(GL_SMOOTH);
-    //GLfloat light_position[] = { -0.50, -0.50,-1.0,0 };
-   //GLfloat light_position[] = { 7, 1,40.0,1 };
-    //GLfloat light_position[] = { 1, 1,1,1 };
-    //GLfloat luz_ambiental[] = { 0.2, 0.2, 0.2, 10.0 };
-    //glEnable(GL_LIGHTING);
-    //glEnable(GL_LIGHT0);
     glDepthFunc(GL_LESS);
     glEnable(GL_DEPTH_TEST);
-  // glLightfv(GL_LIGHT0,GL_POSITION,light_position);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glPushMatrix();
-
 }
 
 void reshape(int w, int h)
